b3f1613d18a5555747d5c76e2ccdbcc4b444fcba
Dave <dave.gonzalez1992@outlook.com>
2024-06-24 11:53:26 -0500

    Nuvos ejercicio set and dict comprehension

---
+++ dict_comprehension_ejercicios.py
@@ -0,0 +1,21 @@
+# Ejercicio 1 --> Dict comprehension
+# A partir del siguiente listado de tuplas.
+# tuples = [('a', 1), ('b', 2), ('c', 3)]
+# Crea un diccionario utilizando una dictionary comprehension.
+
+
+
+#Ejercicio 2 
+# Crea un diccionario con las letras de una palabra y su frecuencia utilizando una dictionary comprehension.
+# Ejemplo.
+
+# >>> name  = 'example'
+# >>> dict
+# {
+#     'e': 2,
+#     'x'; 1,
+#     'a'; 1,
+#     'm'; 1,
+#     'p'; 1,
+#     'l'; 1,
+# }
---
+++ set_comprehension_ejercicios.py
@@ -0,0 +1,4 @@
+# Sets comprehension
+# A partir del siguiente listado de números:
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Crea un conjunto con los elementos que sean múltiplos de 3.
