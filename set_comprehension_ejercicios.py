bf02ec51f5ce8c14b36145c92b488d4a262e6c47
Dave <dave.gonzalez1992@outlook.com>
2024-06-29 16:36:06 -0500

    Set comprehension resuelto

--- set_comprehension_ejercicios.py
+++ set_comprehension_ejercicios.py
@@ -2,3 +2,9 @@
 # A partir del siguiente listado de números:
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Crea un conjunto con los elementos que sean múltiplos de 3.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+multiplos_3 = tuple([num for num in numbers if num % 3 == 0])
+
+print(multiplos_3)
